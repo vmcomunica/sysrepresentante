416d2ee99423766413163aaa5b4b2010da0f802f
vmcomunica <vmcomunicacaodrive@gmail.com>
2026-05-06 12:19:21 -0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = https://sxzyogswvfyvsmplytpi.supabase.co;
-const supabaseAnonKey = sb_publishable_kv-sKwYEJq3M3GT2o5Bwqw_9BnFgOev;
+const supabaseUrl = 'https://sxzyogswvfyvsmplytpi.supabase.co';
+const supabaseAnonKey = 'sb_publishable_kv-sKwYEJq3M3GT2o5Bwqw_9BnFgOev';
 
 export const supabase = supabaseUrl && supabaseAnonKey
   ? createClient(supabaseUrl, supabaseAnonKey)
